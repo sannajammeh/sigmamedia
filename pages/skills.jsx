b51f3e82c8cb8dc8da6ca0446e31105755ce0c84
Mathias Kolberg <mathias.ck2000@gmail.com>
2021-01-10 20:00:58 +0100

    skills

--- pages/skills.jsx
+++ pages/skills.jsx
@@ -6,7 +6,6 @@ import Header from '../components/atoms/Header';
 import Footer from '../components/molecules/Footer';
 import Icon from '../components/atoms/Icon';
 
-
 //Framer Motion
 import { motion } from 'framer-motion';
 import Media from '../utils/media';
@@ -65,15 +64,42 @@ const Skills = () => {
 						<a href="https://reactjs.org/" target="_blank">
 							<img src="/images/react-logo.png" alt="React" />
 						</a>
-						<img src="/images/javascript-logo.png" alt="" />
-						<img src="/images/node-logo.png" alt="" />
-						<img src="/images/html-logo.png" alt="" />
-						<img src="/images/sass-logo.png" alt="" />
-						<img src="/images/photoshop-logo.png" alt="" />
-						<img src="/images/mongodb-logo.png" alt="" />
-						<img src="/images/firebase-logo.png" alt="" />
-						<img src="/images/jquery-logo.png" alt="" />
-						<img src="/images/bootstrap-logo.png" alt="" />
+						<a href="https://www.javascript.com/">
+							<img src="/images/javascript-logo.png" alt="" />
+						</a>
+						<a href="https://nodejs.org/en/about/" target="_blank">
+							<img src="/images/node-logo.png" alt="" />
+						</a>
+						<a href="https://html.com/" target="_blank">
+							<img src="/images/html-logo.png" alt="" />
+						</a>
+						<a href="https://sass-lang.com/" target="_blank">
+							<img src="/images/sass-logo.png" alt="" />
+						</a>
+						<a
+							href="https://www.adobe.com/no/products/photoshop.html?sdid=8JD95K3Q&mv=search&ef_id=Cj0KCQiA6Or_BRC_ARIsAPzuer-S8VJhkYnGLKeeI9RRb1D4L8kukRGqjlZQP9D_WpmvuwRjNc53ijAaAqt4EALw_wcB:G:s&s_kwcid=AL!3085!3!442287443495!e!!g!!photoshop!1471316782!58669001444&gclid=Cj0KCQiA6Or_BRC_ARIsAPzuer-S8VJhkYnGLKeeI9RRb1D4L8kukRGqjlZQP9D_WpmvuwRjNc53ijAaAqt4EALw_wcB"
+							target="_blank"
+						>
+							<img src="/images/photoshop-logo.png" alt="" />
+						</a>
+						<a
+							href="https://www.mongodb.com/cloud/atlas/lp/try2?utm_source=google&utm_campaign=gs_emea_norway_search_brand_atlas_desktop&utm_term=mongodb&utm_medium=cpc_paid_search&utm_ad=e&utm_ad_campaign_id=10947982929&gclid=Cj0KCQiA6Or_BRC_ARIsAPzuer-wf6GdJVIDISN7b4kVD_pnlDoyxeyAHDeKo1j0mK3wxqIUFWfNl5UaAmK3EALw_wcB"
+							target="_blank"
+						>
+							<img src="/images/mongodb-logo.png" alt="" />
+						</a>
+						<a
+							href="https://firebase.google.com/?gclid=Cj0KCQiA6Or_BRC_ARIsAPzuer-BiN1SljXmcNXn-k3cxNKyvvUvDYB4Lzi99WJ5D75nImbkLDWZ-p8aAmE2EALw_wcB"
+							target="_blank"
+						>
+							<img src="/images/firebase-logo.png" alt="" />
+						</a>
+						<a href="https://jquery.com/" target="_blank">
+							<img src="/images/jquery-logo.png" alt="" />
+						</a>
+						<a href="https://getbootstrap.com/" target="_blank">
+							<img src="/images/bootstrap-logo.png" alt="" />
+						</a>
 					</center>
 				</SkillsImages>
 			</SkillsSection>
@@ -109,9 +135,6 @@ const IconStyle = styled.div`
 	i {
 		transform: scale(2);
 		margin: 1rem;
-	}
-	&:hover {
-		color: ${({ theme }) => theme.palette.purple[200]};
 	}
 `;
 
