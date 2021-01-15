55aa5c3b3adcf5c7f47ede3af82188dc99610429
Anders Haakonsen <anhakonsen@gmail.com>
2021-01-15 23:23:26 +0100

    Added state to contact form

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -5,31 +5,56 @@ import Text from '../components/atoms/Text';
 import Header from '../components/atoms/Header';
 import RowBase from '../components/atoms/Row';
 import Button from '../components/atoms/Button';
+import { useState } from 'react';
 
 const Row = styled(RowBase)`
 	flex-wrap: nowrap;
 `;
 
 const Contact = () => {
+	const [formData, setFormData] = useState({
+		fullName: '',
+		emailAdress: '',
+		title: '',
+		companyName: '',
+		message: '',
+	});
+
+	const handleChange = e => {
+		const { value, name } = e.target;
+		setFormData({ ...formData, [name]: value });
+	};
+
+	const handleSubmit = e => {
+		e.preventDefault();
+		console.log(formData);
+	};
+
 	return (
 		<div>
 			<ContactSection>
 				<Header>
 					<Text variant="headline">Stay In Touch</Text>
 				</Header>
-				<Form method="POST">
+				<Form method="POST" onSubmit={handleSubmit}>
 					<Row>
 						<Input
 							type="text"
 							placeholder="Your Full Name *"
 							required
 							className="sm"
+							name="fullName"
+							value={formData.fullName}
+							onChange={handleChange}
 						></Input>
 						<Input
 							type="email"
 							placeholder="Your Email Adress *"
 							required
 							className="sm"
+							name="emailAdress"
+							value={formData.emailAdress}
+							onChange={handleChange}
 						></Input>
 					</Row>
 					<Row>
@@ -38,11 +63,17 @@ const Contact = () => {
 							placeholder="Your Title *"
 							required
 							className="sm"
+							name="title"
+							value={formData.title}
+							onChange={handleChange}
 						></Input>
 						<Input
 							type="text"
 							placeholder="Your Company Name *"
 							className="sm"
+							name="companyName"
+							value={formData.companyName}
+							onChange={handleChange}
 						></Input>
 					</Row>
 					<Row>
@@ -53,6 +84,9 @@ const Contact = () => {
 							placeholder="Your Message For Us *"
 							required
 							className="lg"
+							name="message"
+							value={formData.message}
+							onChange={handleChange}
 						></Input>
 					</Row>
 					<Row>
